0c6c44c9d5829e530ad93660db047c5fa0e69c9d
Narayanan <nandhusuman1090@gmail.com>
2022-01-04 21:08:14 +0530

    fix(start):client fix

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
-const {sendEmailHandler} = require('./Service/mail.reqhandler')
-
+const { sendEmailHandler } = require("./Service/mail.reqhandler");
+const path = require("path");
 
 app.use(bodyParser.json());
 
-app.post("/api/sendmail", async(req, res) => {
- 
+app.use(express.static(path.join(__dirname, "./profile/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join((__dirname = "./profile/build/index.html")));
+// });
+
+app.post("/api/sendmail", async (req, res) => {
   const status = await sendEmailHandler(req, res);
   await res.status(200).json(status);
-})
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("App started listening the Port", port);
-})
-
+});
