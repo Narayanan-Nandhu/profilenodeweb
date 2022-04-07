942339758abf3959fbfaa20f42060f9432a102f9
Nandhu <51841792+Narayanan-Nandhu@users.noreply.github.com>
2022-04-07 22:58:30 +0530

    chore(update): removed snowfall and updated packagelock

--- profile/src/App.js
+++ profile/src/App.js
@@ -3,14 +3,10 @@ import React from 'react';
 import Profile from "./components/Profile";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "react-toastify/dist/ReactToastify.min.css";
-import Snowfall from "react-snowfall";
 
 function App() {
   return (
     <React.Fragment>
-      <div  className="snow-fall-customise" >
-        <Snowfall color = {"#ff5722"}/>
-      </div>
       <Profile />
     </React.Fragment>
   );
